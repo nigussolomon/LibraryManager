af5c24fba248004cc5e75a1710a64b09d55fabc8
nigussolomon <nigussolomon444@gmail.com>
2023-01-10 20:48:12 +0300

    configured components according to theme

--- LoginFrame.java
+++ LoginFrame.java
@@ -17,26 +17,26 @@ class LoginFrame extends Base implements ActionListener {
         JLabel userLabel = new JLabel("USERNAME");
         userLabel.setBounds(20, 60, 150, 30);
         this.userField = new JTextField();
-        this.userField.setBounds(20, 90, 450, 30);
+        this.userField.setBounds(20, 90, 450, 40);
 
         JLabel passLabel = new JLabel("PASSWORD");
-        passLabel.setBounds(20, 120, 150, 30);
+        passLabel.setBounds(20, 130, 150, 30);
         this.passField = new JPasswordField();
-        this.passField.setBounds(20, 150, 450, 30);
+        this.passField.setBounds(20, 160, 450, 40);
 
         this.login = new JButton("LOGIN");
-        this.login.setBounds(20, 200, 100, 30);
+        this.login.setBounds(20, 210, 100, 40);
 
         JLabel or = new JLabel("OR");
-        or.setBounds(130, 200, 150, 30);
+        or.setBounds(130, 210, 150, 40);
 
         this.reg = new JButton("GO TO REGISTRATION");
-        this.reg.setBounds(157, 200, 180, 30);
+        this.reg.setBounds(160, 210, 210, 40);
         this.reg.addActionListener(this);
 
         JComponent[] components = { title, userLabel, this.userField, passLabel, this.passField, this.login, or, this.reg };
         super.addComp(components, loginFrame);
-        super.init(500, 280, loginFrame);
+        super.init(500, 300, loginFrame);
     }
 
     @Override
--- RegFrame.java
+++ RegFrame.java
@@ -17,29 +17,29 @@ class RegFrame extends Base implements ActionListener {
         JLabel userLabel = new JLabel("USERNAME");
         userLabel.setBounds(20, 60, 150, 30);
         this.userField = new JTextField();
-        this.userField.setBounds(20, 90, 450, 30);
+        this.userField.setBounds(20, 90, 450, 40);
 
         JLabel passLabel = new JLabel("PASSWORD");
-        passLabel.setBounds(20, 120, 150, 30);
+        passLabel.setBounds(20, 130, 150, 30);
         this.passField = new JPasswordField();
-        this.passField.setBounds(20, 150, 450, 30);
+        this.passField.setBounds(20, 160, 450, 40);
 
         JCheckBox check = new JCheckBox("I have agreed to the Terms and Conditions.", true);
-        check.setBounds(16, 180, 300, 30);
+        check.setBounds(20, 200, 350, 30);
 
         this.reg = new JButton("REGISTER");
-        this.reg.setBounds(20, 220, 100, 30);
+        this.reg.setBounds(20, 230, 100, 40);
 
         JLabel or = new JLabel("OR");
-        or.setBounds(130, 220, 150, 30);
+        or.setBounds(130, 230, 150, 40);
 
         this.login = new JButton("GO TO LOGIN");
-        this.login.setBounds(157, 220, 150, 30);
+        this.login.setBounds(160, 230, 150, 40);
         this.login.addActionListener(this);
 
         JComponent[] components = { title, userLabel, this.userField, passLabel, this.passField, check ,this.login, or, this.reg };
         super.addComp(components, regFrame);
-        super.init(500, 300, regFrame);
+        super.init(500, 320, regFrame);
     }
 
     @Override
